c7ece44ede3002ec67493c2e32e676f58e35e4b2
rnalangan <rnalangan@ucsd.edu>
2023-01-25 13:55:23 -0800

    updated methods to make tests pass
    updated methods to make tests pass

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,12 +7,24 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
-	}
+  }
 
+  @Test 
+	public void testReverseInPlace1() {
+    int[] input2 = { 3, 0, 5, 6 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{ 6, 5, 0, 3 }, input2);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed1() {
+    int[] input2 = { 0, 1, 2, 3, 4};
+    assertArrayEquals(new int[]{ 4, 3, 2, 1, 0}, ArrayExamples.reversed(input2));
   }
 }
